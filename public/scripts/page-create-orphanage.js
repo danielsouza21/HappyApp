18475a6e46bb0f80e3cb601867591a066a3a7bbf
danielsouza21 <danielloko1999@hotmail.com>
2020-10-18 13:41:37 -0300

    aula03 3

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -1,3 +1,4 @@
+/* Map */
 var map = L.map("mapid").setView([-19.8500051, -43.9827995], 14);
 var sizeicon = 40;
 var marker; //have a single marker on map
@@ -29,3 +30,70 @@ map.on("click", function (event) {
   document.querySelector("[name=lat]").value = lat;
   document.querySelector("[name=lng]").value = lng;
 });
+
+/* Field upload photo */
+function addPhotoField() {
+  var ContainerPhoto, FieldsContainer, NewFieldContainer;
+  var Input_FieldContainer;
+
+  //container de fotos: #images
+  ContainerPhoto = document.querySelector("#images");
+
+  //container a ser duplicado: .new-image
+  FieldsContainer = document.querySelectorAll(".new-upload");
+
+  //clonar ultima imagem adicionada
+  NewFieldContainer = FieldsContainer[FieldsContainer.length - 1].cloneNode(1);
+
+  console.log(NewFieldContainer);
+
+  Input_FieldContainer = NewFieldContainer.children[0];
+
+  if (!(Input_FieldContainer.value == "")) {
+    //injetar no HTML novo input ".new-upload"
+    Input_FieldContainer.value = "";
+    ContainerPhoto.appendChild(NewFieldContainer);
+  } else {
+    return;
+  }
+}
+
+function deletePhotoField(event) {
+  var ContainerPhoto, FieldsContainer, elementHTML;
+  var DeleteFieldContainer;
+
+  this_elementHTML = event.currentTarget;
+
+  FieldsContainer = document.querySelectorAll(".new-upload");
+
+  if (FieldsContainer.length < 2) {
+    //limpar campo
+    this_elementHTML.parentNode.children[0].value = "";
+  } else {
+    //deleta campo
+    DeleteFieldContainer = this_elementHTML.parentNode;
+    DeleteFieldContainer.remove();
+  }
+}
+
+/* Select yes or no */
+function toggleselect(event) {
+  //pegar botao clicado
+  var this_elementHTML = event.currentTarget;
+
+  //verifica 'sim' ou 'nao'
+  var button_value = this_elementHTML.value;
+
+  //limpar qualquer classe active
+  var buttons_elements = document.querySelectorAll(".button-select button");
+  buttons_elements.forEach((element) => {
+    element.classList.remove("active");
+  });
+
+  //inserir classe active no botao selecionado
+  this_elementHTML.classList.add("active");
+
+  //atualizar input hidden
+  var input = document.querySelector("[name=open_on_weekends]");
+  input.value = button_value;
+}
